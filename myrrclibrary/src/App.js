4d3ced050a868e02c4fc06d38984ba55c1bf7c28
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-06-26 15:02:54 +0530

    Button component created

--- myrrclibrary/src/App.js
+++ myrrclibrary/src/App.js
@@ -1,3 +1,4 @@
+import Button from './Button';
 import './index.css'
 import Profilecardapp from './Profilecardapp';
 function App() {
@@ -6,6 +7,7 @@ function App() {
       <h1 className="text-3xl font-bold underline flex justify-center mb-10"> This is an app</h1>
      
      <Profilecardapp/>
+     <Button text="This is a button"/>
  
     </div>
   );
---
+++ myrrclibrary/src/Button.js
@@ -0,0 +1,10 @@
+function Button({text}){
+    return(
+<div className="flex justify-center">
+<button>{text}</button>
+</div>
+    );
+
+}
+
+export default Button;
