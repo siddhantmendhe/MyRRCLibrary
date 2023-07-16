331744be791772f616e86e720e3ccc8c9a703bb2
siddhantmendhe <38434303+siddhantmendhe@users.noreply.github.com>
2023-07-16 12:42:06 +0530

    added icon to accordion

--- myrrclibrary/src/Accordion.js
+++ myrrclibrary/src/Accordion.js
@@ -1,15 +1,16 @@
 import { useState } from "react";
-
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 function Accordion({items}){
     const [expandedIndex, setExpandedIndex]=useState(0);
 
 
     const renderedItems=items.map((item, index)=>{
         const isExpanded=index===expandedIndex;
+        const icon= <span>{isExpanded? <GoChevronDown/>:<GoChevronLeft/>}</span>
       
         return(
             <div key={item.id}>
-                <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={()=>setExpandedIndex(index)}> {item.label} </div>
+                <div className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer" onClick={()=>setExpandedIndex(index)}> {item.label} {icon}</div>
                 {isExpanded && <div className="border-b p-5">{item.content}</div>}
             </div>
         )
